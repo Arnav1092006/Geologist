4649d97f06d05280215914d68e715ecd3c732dfb
Arnav1092006 <81370785+Arnav1092006@users.noreply.github.com>
2021-05-07 09:48:54 +0530

    Add files via upload

---
+++ Rubber.js
@@ -0,0 +1,36 @@
+class Rubber{
+	constructor(x,y,r)
+	{
+	// assign options to the rubber ball
+	 
+       var options = 
+	   {
+		'density':1,
+		'friction': 6,
+		'restitution':0.5
+	   }
+
+	   this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			//draw the ellipse here to display the rubber ball
+            //ellipseMode(RADIUS);
+			ellipse(0,0,this.r,this.r);
+
+			pop()
+	}
+
+}
